ef67e2f26259f1c2bd0b0f144fd574036523cef6
angeloarcillas <angeloarcillas512@gmail.com>
2021-01-13 13:42:49 +0800

    wip

--- App/Controllers/PagesController.php
+++ App/Controllers/PagesController.php
@@ -8,33 +8,4 @@ class PagesController
     {
         return view('welcome');
     }
-
-    public function home()
-    {
-        return view('home');
-    }
-
-    public function login()
-    {
-        if (session('logged')) {
-            redirect('/pg/home');
-        }
-
-        return view('auth/login');
-    }
-
-    public function register()
-    {
-        if (session('logged')) {
-            redirect('/pg/home');
-        }
-
-        return view('auth/register');
-    }
-
-    public function logout()
-    {
-        session_destroy();
-        return redirect('/pg');
-    }
 }
