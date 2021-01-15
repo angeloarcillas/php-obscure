f2370a18e0bd1795e80a2c4d9d3937659c9698c8
angeloarcillas <angeloarcillas512@gmail.com>
2021-01-15 11:33:21 +0800

    wip

--- App/Views/includes/_header.php
+++ App/Views/includes/_header.php
@@ -46,11 +46,11 @@
                             <div
                                 class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="#"
+                                <a href="/php-obscure"
                                     class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
 
-                                <a href="#"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                <a href="/php-obscure/users"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Users</a>
 
                                 <a href="#"
                                     class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
