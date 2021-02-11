5f8c1ca4e78f2df6ec69090a41c5541e76f27054
angeloarcillas <angeloarcillas512@gmail.com>
2021-02-11 09:08:08 +0800

    :fire: fix foldername case sensitive

--- autoload.php
+++ autoload.php
@@ -2,9 +2,11 @@
 /**
  * ! Autoloading classes for NAMESPACE only
  */
-spl_autoload_register(function ($className) {
-    $path = dirname(__FILE__) . "/" . str_replace("\\", "/", $className) . ".php";
-
+spl_autoload_register(function ($class) {
+    // $class = strtolower($class);
+    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+    $path = dirname(__FILE__) . DIRECTORY_SEPARATOR ."{$class}.php";
+    // dd($path);
     if (file_exists($path)) {
         require  $path;
     }
